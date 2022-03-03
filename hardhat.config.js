267c24cee4bc0c4aaf7da77090de757f7449020d
gkvpp1876 <gkvpp1876@gmail.com>
2022-03-03 22:17:23 +0530

    hardhat configurations

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,16 @@ require("@nomiclabs/hardhat-waffle");
 
 
 module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: ""
+    },
+    mainnet:{
+      url:""
+    }
+  },
   solidity: "0.8.4",
 };
