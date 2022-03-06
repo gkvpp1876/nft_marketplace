f15ab7b2c417f49c747a65a4e300ce1149588c84
gkvpp1876 <gkvpp1876@gmail.com>
2022-03-06 23:22:03 +0530

    refactering based on v2 changes and Unit testing

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -1,27 +1,25 @@
 //SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract NFTMarket is ReentrancyGuard {
+contract NFTMarket is ERC721URIStorage {
     using Counters for Counters.Counter;
-    Counters.Counter private _itemIds;
-    Counters.Counter private _itemSold;
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
 
     //to make the owner get paid on each transaction
+    //payable modifier means transaction can be processed with non-zero ether value 
     address payable owner;
     uint256 listingPrice = 0.25 ether; // currently we're using Matic token for transaction;
 
-    constructor() {
+    constructor() ERC721("Metaverse Token", "METT"){
         owner = payable(msg.sender);
     }
 
     struct MarketItem {
-        uint256 itemId;
-        address nftContract;
         uint256 tokenId;
         address payable seller;
         address payable owner;
@@ -32,8 +30,6 @@ contract NFTMarket is ReentrancyGuard {
     mapping(uint256 => MarketItem) private idToMarketItem; //mapping allow us to fetch the record with the itemId
 
     event MarketItemCreated(
-        uint indexed itemId,
-        address indexed nftContract,
         uint256 indexed tokenId,
         address seller,
         address owner,
@@ -45,44 +41,51 @@ contract NFTMarket is ReentrancyGuard {
         return listingPrice;
     }
 
+    function updateListingPrice(uint _listingPrice) public payable {
+        require(owner == msg.sender, "Onlu marketplace owner can update the listing price");
+        listingPrice = _listingPrice;
+    }
+
+    function createToken(string memory tokenURI, uint256 price) public payable returns (uint){
+        _tokenIds.increment(); //increments or create new token Id when new NFT is minted
+        uint256 newTokenId = _tokenIds.current(); //getting the current token ID
+
+        _mint(msg.sender, newTokenId); //Mints the newTokenId to the requester/sender
+        _setTokenURI(newTokenId, tokenURI); //setting the tokenURI with the newTokenId identifier 
+        createMarketItem(newTokenId, price);
+        return newTokenId; //returning the newTokenId as we get mint the NFT we'll show in the marketplace for sale so newTokenId helps in reference or identifier
+    }
+
     /*
     This createMarketItem is to Create a NFT to be place for sale
         //nonReentrant is a modifier to prevent from the reentranct attack
         //_itemIds.increment() each item we place for sale needs an Identity and it'll be incremented each time
     */
     function createMarketItem(
-        address nftContract,
         uint256 tokenId,
         uint256 price
-    ) public payable nonReentrant {
+    ) public payable {
         require(price > 0, "Price must be at least 1 wei");
         require(
             msg.value == listingPrice,
             "Price must be equal to listing price"
         );
 
-        _itemIds.increment();
-        uint256 itemId = _itemIds.current();
-
-        idToMarketItem[itemId] = MarketItem(
-            itemId,
-            nftContract,
+        idToMarketItem[tokenId] = MarketItem(
             tokenId,
             payable(msg.sender),
-            payable(address(0)),
+            payable(address(this)),
             price,
             false
         );
 
         //Transfering the ownership to the contract from the creator or the owner of the NFT
-        IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
+        _transfer(msg.sender, address(this), tokenId);
 
         emit MarketItemCreated(
-            itemId,
-            nftContract,
             tokenId,
             msg.sender,
-            address(0),
+            address(this),
             price,
             false
         );
@@ -91,26 +94,111 @@ contract NFTMarket is ReentrancyGuard {
     /*
 
     */
-    function createMarketSale(address nftContrct, uint256 itemId)
+    function createMarketSale(uint256 tokenId)
         public
         payable
-        nonReentrant
     {
-        uint price = idToMarketItem[itemId].price;
-        uint tokenId = idToMarketItem[itemId].tokenId;
+        uint price = idToMarketItem[tokenId].price;
+        address seller = idToMarketItem[tokenId].seller;
 
         require(
             msg.value == price,
             "Please submit the asking price i order to complete the purchase"
         );
 
-        idToMarketItem[itemId].seller.transfer(msg.value); //transfering the value of token from seller to owners address
-        IERC721(nftContrct).transferFrom(address(this), msg.sender, tokenId); //transfering the token from contract to the buyer
-        idToMarketItem[itemId].owner = payable(msg.sender); //setting the owner value to the sender who bought the token
-        idToMarketItem[itemId].sold = true; //setting the mapping to sold
-        _itemSold.increment(); //incrementing the number of tokens sold
+        idToMarketItem[tokenId].owner = payable(msg.sender); //assigning owner as sender while buying the token
+        idToMarketItem[tokenId].sold = true;
+        idToMarketItem[tokenId].seller = payable(address(0));
+        _itemsSold.increment(); //incrementing the number of tokens sold
+        _transfer(address(this), msg.sender, tokenId);
         payable(owner).transfer(listingPrice); //paying the owner of the contract comissions of market place
+        payable(seller).transfer(msg.value);
+    }
 
+    /*
+    Ideally resellToken is selling back the token to the nftmarket contract
+     */
+    function resellToken(uint256 tokenId, uint256 price)
+        public
+        payable
+    {
+        require(idToMarketItem[tokenId].owner == msg.sender, "Only item owner can perform this operation");
+        //real-time scenario needs to be observed if the tokens are generated and is being sold back to the contract what value should be allowed?
+        require(
+            msg.value == listingPrice,
+            "Price must be equal to the listing price"
+        );
 
+        idToMarketItem[tokenId].owner = payable(address(this)); //assigning owner as contract while selling the token
+        idToMarketItem[tokenId].sold = false;
+        idToMarketItem[tokenId].seller = payable(msg.sender);
+        idToMarketItem[tokenId].price = price;
+        _itemsSold.decrement(); //decreasing the number of tokens sold
+
+        _transfer(msg.sender, address(this), tokenId);
+    }
+
+    /*
+
+    memory: is like Computer RAM, data will be wiped off after the execution
+    */
+    function fetchMarketItems() public view returns (MarketItem[] memory) {
+        uint itemCount = _tokenIds.current();
+        uint unsoldItemCount = _tokenIds.current() - _itemsSold.current();
+        uint currentIndex = 0;
+
+        MarketItem[] memory items = new MarketItem[](unsoldItemCount);
+        for (uint i = 0; i < itemCount; i++) {
+            if(idToMarketItem[i + 1].owner == address(this)){
+                uint currentId = i + 1;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+        return items;
+    }
+
+    //getting the tokens purchased by the user
+    function fetchMyNFTs() public view returns (MarketItem[] memory) {
+        uint totalItemCount = _tokenIds.current();
+        uint itemCount = 0;
+        uint currentIndex =0;
+
+        for(uint i = 1; i <= totalItemCount; i++){
+            if(idToMarketItem[i].owner == msg.sender){
+                itemCount += 1;
+            }
+        }
+
+        MarketItem[] memory items = new MarketItem[](itemCount);
+        for(uint i = 1; i <= totalItemCount; i++){
+            MarketItem storage currentItem = idToMarketItem[i];
+            items[currentIndex] = currentItem;
+            currentIndex += 1;
+        }
+        return items;
+    }
+
+    function fetchItemsCreated() public view returns (MarketItem[] memory) {
+        uint totalItemCount = _tokenIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+
+        for(uint i = 1; i <= totalItemCount; i++){
+            if(idToMarketItem[i].seller == msg.sender){
+                itemCount += 1;
+            }
+        }
+
+        MarketItem[] memory items = new MarketItem[](itemCount);
+        for(uint i = 1; i <= totalItemCount; i++){
+            if(idToMarketItem[i].seller == msg.sender){
+                MarketItem storage currentItem = idToMarketItem[i];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+        return items;
     }
 }
--- test/sample-test.js
+++ test/sample-test.js
@@ -19,15 +19,6 @@ describe("NFTMarket", function () {
     }).then(async function (address) {
       // console.log('market Address', address);
       assert.notEqual(address, 0x0, 'has contract address');
-      const NFT = await ethers.getContractFactory("NFT");
-      nft = await NFT.deploy(address);
-      return nft.deployed();
-    }).then(function (instance) {
-      nftInstance = instance;
-      return nftInstance.address;
-    }).then(function (address) {
-      // console.log('NFT Address', address);
-      assert.notEqual(address, 0x0, 'has contract address');
       return marketInstance.owner;
     }).then(function (address) {
       assert.notEqual(address, 0x0, 'had owner address');
@@ -40,28 +31,35 @@ describe("NFTMarket", function () {
     }).then(function (auctionPrice) {
       // console.log(auctionPrice);
       assert.equal(auctionPrice, 250000000000000000, 'auction price of the ether should be equal to the listing price set in market contract 0.25 ether');
-      return nft.createToken("https://www.mytokenlocation.com");
-    }).then(function (receipt) {
-      assert.equal(receipt.to, nftInstance.address, 'to address is the nft contract address');
-      assert.equal(receipt.value.toNumber(), 0, 'value of the NFT is 0 when created');
-      return marketInstance.createMarketItem(nftInstance.address, 1, 0, { value: listingPrice });
-    }).then(assert.fail).catch(function (error) {
-      assert(error.message.indexOf('revert') >= 0, 'Price must be greater than 1 wei');
-      return marketInstance.createMarketItem(nftInstance.address, 1, auctionPrice, { value: listingPrice });
+      return marketInstance.createToken("https://www.mytokenlocation.com", auctionPrice, { value: listingPrice });
     }).then(async function (receipt) {
-      assert.equal(receipt.to, marketInstance.address, 'NFT is transfered to market contract with base price as 0.25 ether')
-      assert.equal(receipt.value, 250000000000000000, 'value of nft on market should be equal to listing price');
-      // return market.balanceOf(marketInstance.address);
-      // return market.owner;
+      assert.equal(receipt.to, marketInstance.address, 'to address is the nft contract address');
+      assert.equal(receipt.value, 250000000000000000, 'value of the NFT is 0 when created');
+      await marketInstance.createToken("https://www.mytokenlocation2.com", auctionPrice, { value: listingPrice });
       [_, buyerAddress] = await ethers.getSigners();
-      // console.log(buyerAddress);
       return [_, buyerAddress];
     }).then(function (address) {
-      // console.log(address[1].address);
       assert.notEqual(address[1].address, 0x0, 'had valid buyer address');
-      return market.connect(address[1]).createMarketSale(nftInstance.address, 1, { value: auctionPrice });
-    }).then(function (receipt) {
-      console.log('to check here on the to address and from address while doing the sale and validate the transaction',receipt);
+      return market.connect(address[1]).createMarketSale(1, { value: auctionPrice });
+    }).then(async function (receipt) {
+      //to check this having some doubt
+      // console.log(receipt);
+      assert.equal(receipt.to, marketInstance.address, 'NFT is transfered to market contract with base price as 0.25 ether')
+      assert.equal(receipt.value, 250000000000000000, 'value of nft on market should be equal to listing price');
+
+      let items = await market.fetchMarketItems();
+      items = await Promise.all(items.map(async i => {
+        const tokenUri = await market.tokenURI(i.tokenId)
+        let item = {
+          price: i.price.toString(),
+          tokenId: i.tokenId.toString(),
+          seller: i.seller,
+          owner: i.owner,
+          tokenUri
+        }
+        return item;
+      }))
+      console.log(items);
     });
 
   });
